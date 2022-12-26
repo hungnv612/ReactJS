c6e8de97cec7a85d16dfb43c8e124eb899e440a5
hungnv612 <hungdaubasg@gmail.com>
2022-12-26 09:00:27 +0700

    update

--- List.js
+++ List.js
@@ -4,35 +4,102 @@ class List extends React.Component {
     constructor() {
         super();
         this.state = {
-            data:
-                [
+            index: 0,
+            name: '',
+            age: 0,
+            action: 'ADD ITEM',
+            people: [
                     {
-                        "id": 1,
-                        "name": "hung",
-                        "age": 20
+                        name: "hung",
+                        age: 20
                     },
                     {
-                        "id": 2,
-                        "name": "nam",
-                        "age": 30
+                        name: "nam",
+                        age: 30
                     },
                     {
-                        "id": 3,
-                        "name": "nu",
-                        "age": 40
+                        name: "nu",
+                        age: 40
                     }
                 ]
         }
+        this.changeName = this.changeName.bind(this);
+        this.changeAge = this.changeAge.bind(this);
     };
 
-        handleDelete = (item) => {
-            const newsId = item.id;
-            console.log(newsId);
+    changeName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    changeAge = (e) => {
+        this.setState({
+            age: e.target.value
+        })
+    }
+    addItem = () => {
+        if (!this.state.people.find(item => item.name === this.state.name)) {
+            this.setState({
+                people: [
+                    ...this.state.people,
+                    {name: this.state.name, age: this.state.age}
+                ],
+                name: "",
+                age: ""
+            })
         }
+    }
+    deleteItem = (name) => this.setState({
+        people: this.state.people.filter(item => item.name !== name)
+    })
+    edit = (item, index) => {
+        this.setState({
+            action: 'UPDATE ITEM',
+            name: item.name,
+            age: item.age,
+            index: index
+        })
+    }
+
+    updateItem = () => {
+        let data = this.state.people;
+        data.map((item, index) => {
+            if (this.state.index === index) {
+                item.name = this.state.name;
+                item.age = parseInt(this.state.age);
+            }
+        })
+
+        this.setState({
+            laptop: data,
+            name: '',
+            age: '',
+            action: 'ADD ITEM'
+        })
+    }
 
     render() {
         return (
             <div>
+                <div >
+                    <h1>{this.state.action}</h1>
+                    <div>
+                        <label>Name</label>
+                        <input type="text" name="" onChange={this.changeName}
+                               value={this.state.name}/>
+                    </div>
+                    <div>
+                        <label>age</label>
+                        <input type="text" name="" onChange={this.changeAge}
+                               value={this.state.age}/>
+                    </div>
+                    <div >
+                        <button type="button"
+                                onClick={this.state.action === 'ADD ITEM' ? this.addItem : this.updateItem}>Add
+                        </button>
+                    </div>
+                </div>
                 <table>
                     <tbody>
                     <td>STT</td>
@@ -40,13 +107,19 @@ class List extends React.Component {
                     <td>AGE</td>
                     <td>UPDATE</td>
                     <td>DELETE</td>
-                    {this.state.data.map((person,index) =>
+                    {this.state.people.map((person,index) =>
                         <tr>
                             <td>{index+1}</td>
                             <td>{person.name}</td>
                             <td>{person.age}</td>
-                            <td><button>update</button></td>
-                            <td><button>delete</button></td>
+                            <td>
+                                <button
+                                        onClick={() => this.edit(person, index)}>Edit
+                                </button>
+                            </td>
+                            <button
+                                    onClick={() => this.deleteItem(person.name)}>Delete
+                            </button>
                         </tr>)}
                     </tbody>
                 </table>
